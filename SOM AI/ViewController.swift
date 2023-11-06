ac061f830bd6a2f4e88c7952f24697630db97205
Bagas Ilham <bagas.ilham@ottencoffee.co.id>
2023-11-06 12:59:58 +0700

    Initial Commit

---
+++ SOM AI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SOM AI
+//
+//  Created by Bagas Ilham on 06/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
